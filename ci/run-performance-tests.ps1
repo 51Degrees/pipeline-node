7d9b8095d793ad94b38ccdcdcda4f90cf9bf033a
YaroslavVlasenko <yaroslavvlasenko@gmail.com>
2026-09-16 18:42:15 +0300

    Update ESLint config to ignore JavaScript templates and simplify performance tests script

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,8 +1,1 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName
-)
-
-./node/run-performance-tests.ps1 -RepoName $RepoName
-
-exit $LASTEXITCODE
+Write-Output "There are no performance tests"
